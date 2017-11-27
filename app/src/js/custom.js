90c1cca7cb3cdb0b3488d094cd4e681ea1f3c178
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2017-11-26 23:41:49 -0500

    rendered pages and added survey section

--- app/src/js/custom.js
+++ app/src/js/custom.js
@@ -1,1 +1,30 @@
-//custom.js
+window.onload=function(){
+	//showHomeSection()
+
+	//Add event listener
+	document.body.addEventListener('click', function (event) {
+		if (event.target.dataset.section) {
+			hideAllSections()
+			showSection(event)
+		}
+	})
+
+}
+
+// hides all sections from view
+function hideAllSections(){
+	//document.querySelector(#) + ' section').hide()
+}
+
+//show section on click
+function showSection(event){
+	const sectionId = event.target.dataset.section
+	$('#' + sectionId).show()
+	$('#' + sectionId + ' section').show()
+}
+
+// show main window only on window.load()
+function showHomeSection(){
+	//..
+}
+
--- app/src/js/renderer.js
+++ app/src/js/renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+
+const links = document.querySelectorAll('link[rel="import"]')
+
+// Import and add each page to the DOM
+Array.prototype.forEach.call(links, function (link) {
+	let template = link.import.querySelector('.win-template')
+	let clone = document.importNode(template.content, true)
+	document.querySelector('#window').appendChild(clone)
+})
+
