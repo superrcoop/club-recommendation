265f2f39216133e603c14dbe761a0605070c8a64
Jordan Cooper <superrcoop@Jordans-MacBook-Pro.local>
2017-11-27 13:04:55 -0500

    added list of clubs,settings,login-signup and FAQ pages + more imgs

--- app/src/js/custom.js
+++ app/src/js/custom.js
@@ -1,6 +1,8 @@
-window.onload=function(){
-	//showHomeSection()
 
+//var club = ["Football","Cricket","UWI Choir","Uwi Dance society","Uwi Visual Arts society","Uwi Computing society","Uwi Law society","Rotaract Club"]
+window.onload=function(){
+	//showFrontSectionOnly()
+	
 	//Add event listener
 	document.body.addEventListener('click', function (event) {
 		if (event.target.dataset.section) {
@@ -8,6 +10,27 @@ window.onload=function(){
 			showSection(event)
 		}
 	})
+	//survey analysis 
+	$('[data-loading-start]').click(function() {
+	  $(this).addClass('hide');
+	  $(this).parent().find('[data-loading-end]').removeClass('hide');
+	  setTimeout(function() {
+	    $('[data-loading-start]').removeClass('hide');
+	    $('[data-loading-end]').addClass('hide');
+	    $('[data-success-message]').removeClass('hide')
+	  }, 5000)
+	});
+
+		// Open Card Reveal Click
+	$('.open-button').click(function(){
+	  $(this).siblings('.card-reveal').toggleClass('open');
+	});
+
+	// Close Card Reveal Click
+	$('.close-button').click(function(){
+	  $(this).parent().parent('.card-reveal').toggleClass('open');
+	});
+
 
 }
 
@@ -24,7 +47,16 @@ function showSection(event){
 }
 
 // show main window only on window.load()
-function showHomeSection(){
+function showHomeSectionOnly(){
 	//..
 }
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
 
